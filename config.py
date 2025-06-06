f09121ed3e64477853cb2b279f092c12500531a0
Sandeep Poudel <sp2596@ag-bee-h4vrw5x.lan>
2025-06-06 16:33:01 -0400

    device -> cuda or mps or cpu

--- config.py
+++ config.py
@@ -16,7 +16,11 @@ hparams = {
 }
 
 # Device
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = (
+    torch.device("cuda") if torch.cuda.is_available()
+    else torch.device("mps") if torch.backends.mps.is_available()
+    else torch.device("cpu")
+)
 
 # Paths and filenames (now in UPPERCASE)
 DATA_FOLDER = "data/input"
