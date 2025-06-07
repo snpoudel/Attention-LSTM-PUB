69491bbba2332e48e7181a6ab99ac3c309be031e
Sandeep Poudel <sp2596@ag-bee-h4vrw5x.lan>
2025-06-07 16:55:53 -0400

    these scripts are not used now - maybe needed later in the line

--- plot.py
+++ plot.py
@@ -1,1 +1,4 @@
 #read funtions from utils and make necessary plots
+
+'''Maybe this script is also not needed now, as the train script itself handles plotting
+I can also write nse script insideutils and then directly call it from train script'''
