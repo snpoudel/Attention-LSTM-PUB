09dd7361f181fa025bf70ea167d3de1b50d534f6
Sandeep Poudel <sp2596@ag-bee-h4vrw5x.lan>
2025-06-07 16:55:29 -0400

    config designed to save and process files by fold index

--- config.py
+++ config.py
@@ -9,11 +9,14 @@ hparams = {
     'num_layers': 1,
     'dropout': 0.3,
     'context_dropout': 0.3,
-    'num_heads': 4,
+    'num_heads': 2,
     'seq_length': 2,
     'num_epochs': 5,
     'lr': 1e-4
 }
+#test fold index
+#use 0 for tuning, and 1 or 2 or 3 or 4 for training
+TEST_FOLD_INDEX = 0
 
 # Device
 DEVICE = (
@@ -27,5 +30,6 @@ DATA_FOLDER = "data/input"
 BASIN_LIST_FILE = "data/basin_list_with_folds.csv"
 OUTPUT_DIR = "output"
 FIGURE_DIR = "figures"
-MODEL_CKPT_FILE = "best_model.pt"
-ATTENTION_MATRIX_FILE = "best_attention.pt"
+MODEL_CKPT_FILE = f"{TEST_FOLD_INDEX}best_model.pt"
+ATTENTION_MATRIX_FILE = f"{TEST_FOLD_INDEX}best_attention.pt"
+
